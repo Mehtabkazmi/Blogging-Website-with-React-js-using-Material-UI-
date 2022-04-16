03a7cd23e745ef95968691655a2b1041c8b78bf6
Mehtabkazmi <mehtabkazmi5@gmail.com>
2022-04-16 19:42:40 +0500

    first update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+}
+
+export default App;
